37b7d0299e39ee945a4333d2a61a4696109f0911
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-02 23:49:50 +0600

    object

---
+++ object.js
@@ -0,0 +1,20 @@
+const age = 24;
+const school = "hatirjheel";
+const isPassed = true;
+// let isDeveloper;
+const subject = ["bangla", "english", "math", "physics"];
+
+// object 
+
+const bottle = {
+    name: "water",
+    brand: "Samsung",
+    price: 345,
+    isClean: true
+}
+
+const subjects = {
+    name: "math",
+    teacher: "roy",
+    ed: "30feb",
+}
