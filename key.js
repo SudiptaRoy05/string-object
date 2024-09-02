11cd45626b7ad4ede26009e37da9cda72b81c6f3
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-03 00:17:36 +0600

    nested object

--- key.js
+++ key.js
@@ -14,3 +14,28 @@ console.log(keys);
 
 const value = Object.values(computer);
 console.log(value);
+
+
+// nasted object 
+
+const college ={
+    name: "vlc",
+    class: ["11", "12"],
+    events: ["science fair", "Bijoy Dibos", "21 feb"],
+    unique:{
+        color: "Blue",
+        result:{
+            gpa: 5,
+            merit: "top",
+        }
+    }
+}
+
+console.log("]]]]]]]]]]]]]]]]]]]]]]]][[[[[[[[[[[[[[[[[[[[[[[[[[")
+
+console.log(college.unique);
+console.log(college.unique.color);
+console.log(college.unique.result.gpa);
+
+delete college.class;
+console.log(college);
