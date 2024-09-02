c49475708f31458b7510a0fb0a4382011e62ba00
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-03 00:03:06 +0600

    value

---
+++ value.js
@@ -0,0 +1,13 @@
+const person = {
+    name: "Sudipta",
+    age: 25,
+    profession: "developer",
+    salary: 48000,
+    married: false
+}
+
+console.log(person)
+person.salary = 50000;
+person["age"] = 26;
+
+console.log(person);
