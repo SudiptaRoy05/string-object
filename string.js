5640b92492977511e78898acb97b4320b4237f00
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-02 22:37:36 +0600

    string01

--- string.js
+++ string.js
@@ -1,1 +1,9 @@
 const country = 'bangladesh';
+const division = 'Dhaka';
+const district = 'Gopalganj';
+const thana = new String('Kotalipara');
+
+console.log(country)
+
+console.log(country.length);
+console.log(country[5]);
