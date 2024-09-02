2b5db446c6b6e1f955cf4914be32346af73f8082
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-09-03 00:07:36 +0600

    key-value

---
+++ key.js
@@ -0,0 +1,16 @@
+const computer = {
+    brand: "asus",
+    price: 57000,
+    processor: "intel",
+    ssd: "240GB",
+    hdd: "1TB",
+}
+
+
+const keys = Object.keys(computer);
+console.log(keys);
+
+// values 
+
+const value = Object.values(computer);
+console.log(value);
